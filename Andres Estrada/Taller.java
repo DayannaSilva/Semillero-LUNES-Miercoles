b3d85b60a522879e9465c255e3c86f898cad5f57
AndrewHacks <49221016+AndrewHacks@users.noreply.github.com>
2019-06-22 23:36:49 -0500

    Add files via upload
    He hecho todo, sin embargo la última parte de un movimiento no la entendí muy bien, y es lo único que me faltó.

--- Andres Estrada/Taller.java
+++ Andres Estrada/Taller.java
@@ -21,6 +21,7 @@ public class Taller {
         Scanner sc= new Scanner(System.in);
         String ingresoDato_1;
         boolean flag=false;
+        boolean flag_2=true;
         do
         {
         System.out.print("\nIngrese el dato -> ");
@@ -39,17 +40,70 @@ public class Taller {
         
         }while((isNumeric(ingresoDato_1))==flag && (isLetter(ingresoDato_1))==flag);
         System.out.println("");
+        if(isNumeric(ingresoDato_1)==flag_2)
+        {
+            if(isCapicua(ingresoDato_1)==flag_2)
+            {
+               System.out.println("Es capicua");
+               String [] stringNumber=ingresoDato_1.split("");
+               int suma=0;
+               
+               for(int i=0;i<stringNumber.length;i++)
+               {
+                 int arrayNumberCapicua[]=new int[stringNumber.length];
+                 arrayNumberCapicua[i]=Integer.parseInt(stringNumber[i]);
+                 suma=suma+arrayNumberCapicua[i];
+                 
+               }
+                System.out.println("La suma del numero capicua "+ingresoDato_1+" es "+suma);
+                System.out.println("");
+                
+               
+            }
+            else
+            {
+               int numberPlus;
+               int iPosicion=0;
+
+               String [] stringNumber=ingresoDato_1.split("");
+               int saveData=Integer.parseInt(stringNumber[0]);
+                System.out.println("");
+                System.out.print("El arreglo mayor es:");
+               for(int i=0;i<stringNumber.length;i++)
+               {
+                 int arrayNumberCapicua[]=new int[stringNumber.length];
+                 arrayNumberCapicua[i]=Integer.parseInt(stringNumber[i]);
+                 numberPlus=arrayNumberCapicua[0];
+
+                 for(int j=1;j<stringNumber.length;j++)
+                 {
+                     if(arrayNumberCapicua[j]>numberPlus)
+                     {
+                         numberPlus=arrayNumberCapicua[j];
+                         iPosicion=j;
+                         System.out.printf("%d",numberPlus);
+                         
+                     }
+                 }
+                 
+               }
+               System.out.printf("%d",saveData);
+               System.out.println("");
+
+
+                
+            }
+            
+                
+            
+        }
+
+            
+        
+        
         // TODO code application logic here
     }
     
-/*public static boolean comprobar_capicua(String cadena)
-{
-    String [] stringCadena=cadena.split("");
-    for(int i=0;i<=cadena.length;i++)
-    {
-        
-    }
-}*/
     
 public static boolean isNumeric(String cadena)
 {
@@ -71,6 +125,23 @@ public static boolean isLetter(String cadena)
         return false;
   
 }
+public static boolean isCapicua(String cadena)
+{
+    String cadenaInvertida= new String("");
+    for (int i=cadena.length()-1;i>=0;i--)
+    {
+        cadenaInvertida = cadenaInvertida + cadena.charAt(i);
+    }
+    
+    if(cadena.equals(cadenaInvertida))
+    {
+    return true;
+    }
+    else
+    return false;
+    
+}
+        
 
 }
 
